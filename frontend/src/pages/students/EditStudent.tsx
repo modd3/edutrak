7786c98a2da9e9c43679fe72d08201c6edfd59c3
modd3 <dimoreranx008@gmail.com>
2025-11-08 07:20:16 +0300

    Create EditStudent component for editing a student.

---
+++ frontend/src/pages/students/EditStudent.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const EditStudent = () => {
+  return (
+    <div>
+      <h1>Edit Student</h1>
+      {/* Add your form and logic for editing a student here */}
+    </div>
+  );
+};
+
+export default EditStudent;
