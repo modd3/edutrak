8f9fc39b12480bcb7795dd010e0e968cbdf5d89f
modd3 <dimoreranx008@gmail.com>
2026-06-16 09:47:11 +0200

    Minor bug fix

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,12 +3,14 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 
 export default defineConfig({
+  base: './',
   plugins: [react()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
     },
   },
+  
   server: {
     port: 3000,
     proxy: {
